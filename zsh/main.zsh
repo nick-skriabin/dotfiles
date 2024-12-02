9d9eda0de3c02a44c72a313da3c30d93cc690eef
Nick Skriabin <nr@fenelon.ru>
2024-12-02 05:09:46 +0000

    Fix starship

--- zsh/main.zsh
+++ zsh/main.zsh
@@ -6,8 +6,15 @@ export NVM_DIR="$HOME/development/nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-eval "$(/opt/homebrew/bin/brew shellenv)"
 eval "$(zoxide init zsh)"
+
+# Check that the function `starship_zle-keymap-select()` is defined.
+# xref: https://github.com/starship/starship/issues/3418
+type starship_zle-keymap-select >/dev/null || \
+{
+  echo "Load starship"
+  eval "$(/usr/local/bin/starship init zsh)"
+}
 
 # Set up fzf key bindings and fuzzy completion
 source <(fzf --zsh)
